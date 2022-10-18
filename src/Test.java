e9c341b334edeeb16658fa7468c3fd1ca91f9a06
Denys-Lutsenko <d.r.lutsenko@student.khai.edu>
2022-10-18 18:31:38 +0200

    Test.java added to feature_branch with change on string #3

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println( & quot; education & quot;);
+        System.out.println( " education");
+        //new string
+        System.out.println("DICT");
     }
 }
