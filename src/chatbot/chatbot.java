56284a39fd5d486a135ae96997a81489a9187c40
Denys-Lutsenko <d.r.lutsenko@student.khai.edu>
2022-10-20 15:37:16 +0200

    ChatBot 2-st

--- src/chatbot/chatbot.java
+++ src/chatbot/chatbot.java
@@ -1,10 +1,21 @@
 package chatbot;
+import java.util.Scanner;
 
 public class chatbot {
 
         public static void main(String[] args) {
             System.out.println("Hello! My name is {bot_name}.");
             System.out.println( "I was created in 2020");
+            System.out.println( "Please, remind me your name: ");
+            Scanner input = new Scanner(System.in);
+            String your_name = input.next();
+            System.out.println( "What a great name you have," + your_name +"!" );
+
+
+
+
+
+
         }
     }
 
