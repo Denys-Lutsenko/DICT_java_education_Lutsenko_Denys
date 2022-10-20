a50b1f9194fe38dbd20e58dfda7681b4689d90bd
Denys-Lutsenko <d.r.lutsenko@student.khai.edu>
2022-10-20 15:17:46 +0200

    ChatBot 1-st

---
+++ src/chatbot/chatbot.java
@@ -0,0 +1,11 @@
+package chatbot;
+
+public class chatbot {
+
+        public static void main(String[] args) {
+            System.out.println("Hello! My name is {bot_name}.");
+            System.out.println( "I was created in 2020");
+        }
+    }
+
+
